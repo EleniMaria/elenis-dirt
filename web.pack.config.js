ff438587e8364421e307ffd5ec14a0a9d43ccaa4
Eleni-Maria <egoranites@hotmail.com>
2019-11-18 14:29:23 -0500

    web.pack.config

---
+++ web.pack.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  mode: 'production',
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx']
+  },
+
+  entry:
+    path.join(process.cwd(), 'app/app.tsx'),
+
+  module: {
+    rules: [
+      {
+        rules:[{ test: /\.tsx?$/, loader: 'awesome-typescript-loader' }], 
+        resolve: {extensions: ['.js', '.jsx', '.react.js', '.ts', '.tsx']},
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
+};
